11ddbd682c147fa2b590a2d346a4fec0b727b8d3
Luis <lecarcamol@gmail.com>
2022-04-05 11:33:06 -0300

    Controller login busca no BD e retorna mensagens de status

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ContatosRouter = require('./routes/ContatosRouter')
+var AuthRouter = require('./routes/AuthRouter')
 
 var app = express();
 
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', AuthRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/contatos', ContatosRouter);
